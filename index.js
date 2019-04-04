93c183851a42b05c0d44a6e7c62b6c7bd9901b29
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-04 15:16:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class BoardMember {
   constructor(name, homeState, training) {
-    this.name = name 
-    this.homeState = homeState 
+    this.name = name
+    this.homeState = homeState
     this.training = training
   }
 
